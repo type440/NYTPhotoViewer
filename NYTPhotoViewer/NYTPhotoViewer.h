eff174e47a299c2ed109dc6aa8f8a32c874ccec0
Mark Sands <marksands07@gmail.com>
2016-05-13 22:55:40 -0500

    Carthage port

---
+++ NYTPhotoViewer/NYTPhotoViewer.h
@@ -0,0 +1,33 @@
+//
+//  NYTPhotoViewer.h
+//  NYTPhotoViewer
+//
+//  Created by Mark Sands on 2/5/16.
+//  Copyright © 2016 Mark Sands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NYTPhotoViewer.
+FOUNDATION_EXPORT double NYTPhotoViewerVersionNumber;
+
+//! Project version string for NYTPhotoViewer.
+FOUNDATION_EXPORT const unsigned char NYTPhotoViewerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+//#import <NYTPhotoViewer/PublicHeader.h>
+
+#import <NYTPhotoViewer/NYTPhotoCaptionView.h>
+#import "NYTPhotoDismissalInteractionController.h"
+#import "NYTPhotosDataSource.h"
+#import "NYTPhotosOverlayView.h"
+#import "NYTPhotosViewController.h"
+#import "NYTPhotoTransitionAnimator.h"
+#import "NYTPhotoTransitionController.h"
+#import "NYTPhotoViewController.h"
+#import "NYTScalingImageView.h"
+#import "NYTPhoto.h"
+#import "NYTPhotoCaptionViewLayoutWidthHinting.h"
+#import "NYTPhotoContainer.h"
+#import "NYTPhotosViewControllerDataSource.h"
+#import "NSBundle+NYTPhotoViewer.h"
